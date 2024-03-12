56a8cc16bb3502c59f9e9d300468d257ec146873
Futheme <kyoro_souza@hotmail.com>
2024-03-12 16:01:20 -0300

    🚀 init project

---
+++ panda.config.ts
@@ -0,0 +1,42 @@
+import { defineConfig } from "@pandacss/dev";
+import {
+  colors,
+  fonts,
+  fontWeights,
+  fontSizes,
+  radii,
+  breakpoints,
+} from "@stage-lib/tokens";
+
+export default defineConfig({
+  preflight: true,
+  jsxFramework: "react",
+
+  theme: {
+    extend: {
+      breakpoints,
+      tokens: {
+        colors,
+        fontSizes,
+        radii,
+        fonts: {
+          default: {
+            value: fonts.default,
+          },
+          code: {
+            value: fonts.code,
+          },
+        },
+        fontWeights: {
+          light: { value: fontWeights.light },
+          regular: { value: fontWeights.regular },
+          medium: { value: fontWeights.medium },
+          bold: { value: fontWeights.bold },
+          black: { value: fontWeights.black },
+        },
+      },
+    },
+  },
+
+  outdir: "src",
+});
